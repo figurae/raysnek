0a4aa2a22a8e6a2d623822ea678386a1a1fcbcef
figurae <aleksander.gielnicki@gmail.com>
2023-04-13 22:07:03 +0200

    Change Vector2D member names from a/b to x/y

--- src/Math.hxx
+++ src/Math.hxx
@@ -6,53 +6,52 @@
 // TODO: implement second typename
 // and handle casting to different types
 template <typename T> struct Vector2D {
-  T m_a;
-  T m_b;
+  T m_x;
+  T m_y;
 
   Vector2D() = default;
-  Vector2D(T a, T b) : m_a(a), m_b(b) {}
+  Vector2D(T x, T y) : m_x(x), m_y(y) {}
 
-  Vector2D operator+(Vector2D &other) {
-    return Vector2D(m_a + other.m_a, m_b + other.m_b);
+  Vector2D operator+(const Vector2D &other) {
+    return Vector2D(m_x + other.m_x, m_y + other.m_y);
   }
 
-  Vector2D operator-(Vector2D &other) {
-    return Vector2D(m_a - other.m_a, m_b - other.m_b);
+  Vector2D operator-(const Vector2D &other) {
+    return Vector2D(m_x - other.m_x, m_y - other.m_y);
   }
 
   // TODO: make sure this produces correct types
-  Vector2D operator*(T scalar) { return Vector2D(m_a * scalar, m_b * scalar); }
-
+  Vector2D operator*(T scalar) { return Vector2D(m_x * scalar, m_y * scalar); }
   Vector2D operator/(T scalar) { return this * (1 / scalar); }
 
-  Vector2D &operator+=(Vector2D &other) {
-    m_a += other.m_a;
-    m_b += other.m_b;
+  Vector2D &operator+=(const Vector2D &other) {
+    m_x += other.m_x;
+    m_y += other.m_y;
     return *this;
   }
 
-  Vector2D &operator-=(Vector2D &other) {
-    m_a -= other.m_a;
-    m_b -= other.m_b;
+  Vector2D &operator-=(const Vector2D &other) {
+    m_x -= other.m_x;
+    m_y -= other.m_y;
     return *this;
   }
 
-  Vector2D &operator*=(T scalar) {
-    m_a *= scalar;
-    m_b *= scalar;
+  Vector2D &operator*=(const T scalar) {
+    m_x *= scalar;
+    m_y *= scalar;
     return *this;
   }
 
-  Vector2D &operator/=(T scalar) {
-    m_a /= scalar;
-    m_b /= scalar;
+  Vector2D &operator/=(const T scalar) {
+    m_x /= scalar;
+    m_y /= scalar;
     return *this;
   }
 
   // TODO: check how to make LSP formatter
   // split this return nicely
   std::string to_string() {
-    return std::string('(' + std::to_string(m_a) + ", " + std::to_string(m_b) +
+    return std::string('(' + std::to_string(m_x) + ", " + std::to_string(m_y) +
                        ')');
   }
 };
@@ -86,7 +85,7 @@ template <typename T> struct Rect {
     std::tie(bC1, bC2) = other.getCoordinates();
 
     // TODO: argh, LSP formatter, y u so ugly
-    return aC1.m_a < bC2.m_a && aC2.m_a > bC1.m_a && aC1.m_b < bC2.m_b &&
-           aC2.m_b > bC1.m_b;
+    return aC1.m_x < bC2.m_x && aC2.m_x > bC1.m_x && aC1.m_y < bC2.m_y &&
+           aC2.m_y > bC1.m_y;
   }
 };
