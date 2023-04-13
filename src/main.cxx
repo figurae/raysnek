4ed9f4581f8082c1071d895af9d6f017f1db74e3
figurae <aleksander.gielnicki@gmail.com>
2023-04-13 22:11:00 +0200

    Adjust main.cxx to new structure

--- src/main.cxx
+++ src/main.cxx
@@ -1,42 +1,19 @@
-#include "Math.hxx"
-#include "config.hxx"
-#include "raylib-cpp.hpp"
-#include <iostream>
+#include "Game.hxx"
+#include <memory>
 
-#if defined(PLATFORM_WEB)
+/* #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
-#endif
-
-int screenWidth = 800;
-int screenHeight = 450;
-
-void Update(void) {
-  // TODO: to be implemented
-}
-
-void Draw(void) {
-  BeginDrawing();
-
-  ClearBackground(BLACK);
-  DrawText("Welcome to RAYSNEK", 190, 200, 20, RAYWHITE);
-
-  EndDrawing();
-}
+#endif */
 
 int main() {
-  raylib::Window window(screenWidth, screenHeight,
-                        (GAME_TITLE + " " + GAME_VERSION));
+  auto game = std::make_unique<Game>();
+  game->run();
 
-#if defined(PLATFORM_WEB)
-  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-#else
-  SetTargetFPS(60);
-
-  while (!window.ShouldClose()) {
-    Update();
-    Draw();
-  }
-#endif
+  /* #if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+  #else
+    // non-web stuff goes here
+  #endif */
 
   return 0;
 }
