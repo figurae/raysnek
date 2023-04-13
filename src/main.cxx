60adb9c9a799cb450c34f0fcd66c17ef758dc2be
figurae <aleksander.gielnicki@gmail.com>
2023-04-13 14:50:23 +0200

    Format main.cxx using LSP formatter

--- src/main.cxx
+++ src/main.cxx
@@ -1,43 +1,41 @@
-#include "raylib-cpp.hpp"
 #include "config.hxx"
+#include "raylib-cpp.hpp"
 #include <iostream>
 
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
 int screenWidth = 800;
 int screenHeight = 450;
-    
+
 void Update(void) {
-    // TODO: to be implemented
+  // TODO: to be implemented
 }
 
 void Draw(void) {
-    BeginDrawing();
+  BeginDrawing();
 
-        ClearBackground(BLACK);
-        DrawText("Welcome to RAYSNEK", 190, 200, 20, RAYWHITE);
+  ClearBackground(BLACK);
+  DrawText("Welcome to RAYSNEK", 190, 200, 20, RAYWHITE);
 
-    EndDrawing();
+  EndDrawing();
 }
 
-int main()
-{
-    raylib::Window window(screenWidth, screenHeight, (GAME_TITLE + " " + GAME_VERSION));
+int main() {
+  raylib::Window window(screenWidth, screenHeight,
+                        (GAME_TITLE + " " + GAME_VERSION));
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
-    SetTargetFPS(60);
+  SetTargetFPS(60);
 
-    while (!window.ShouldClose())
-    {
-        Update();
-        Draw();
-    }
+  while (!window.ShouldClose()) {
+    Update();
+    Draw();
+  }
 #endif
 
-    return 0;
+  return 0;
 }
-
