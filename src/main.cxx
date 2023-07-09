ad3e5818797570f55067b46be99b71911797f169
figurae <aleksander.gielnicki@gmail.com>
2023-07-09 20:57:45 +0200

    chore: remove comments with PLATFORM_WEB stuff

--- src/main.cxx
+++ src/main.cxx
@@ -1,19 +1,9 @@
-
-/* #if defined(PLATFORM_WEB)
-#include <emscripten/emscripten.h>
-#endif */
 #include "Game.hxx" // for Game
 #include <memory>   // for make_unique, unique_ptr
 
 int main() {
   auto game = std::make_unique<Game>();
   game->run();
-
-  /* #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-  #else
-    // non-web stuff goes here
-  #endif */
 
   return 0;
 }
