cf19bf29a6446559cad180dc1f81acc7327a1fa1
figurae <aleksander.gielnicki@gmail.com>
2023-07-09 23:00:59 +0200

    chore: clean up Game declaration

--- src/Game.hxx
+++ src/Game.hxx
@@ -7,21 +7,20 @@
 #include <string>           // for operator+, char_traits, string
 
 class Game {
+public:
+  Game();
+  void run() const;
+
+private:
   int m_screenWidth{800};
   int m_screenHeight{450};
   std::string m_titleBar{GAME_TITLE + ' ' + GAME_VERSION};
   const std::unique_ptr<raylib::Window> m_window{};
-
   const std::unique_ptr<SceneManager> m_sceneManager{};
-
   float m_dT{};
 
-  void initialize() const;
+private:
   void handleInput() const;
   void update() const;
   void draw() const;
-
-public:
-  Game();
-  void run() const;
 };
